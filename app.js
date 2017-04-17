14b58a8c4d8365f70570333a4ab5a643ffdee82d
Rohini Ravindranath <rohinieravi@yahoo.com>
2017-04-17 13:18:51 -0700

    adding javascript

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-// your code here!
+$(function(){
+	$('form').submit(function(event){
+		event.preventDefault();
+		$('.text-report').removeClass('hidden');
+		var wordArray = getTokens($('#user-text').val());
+		var wordCount =wordArray.length;
+		$('.js-wordcount').text(wordCount);
+		var wordLengths={};
+		var totalCharacters = 0;
+		for(var i=0;i<wordArray.length;i++){
+			if(wordArray[i] in wordLengths){
+				totalCharacters+=wordArray[i].length;
+			}
+			else{
+				wordLengths[wordArray[i]]=wordArray[i].length;
+				totalCharacters+=wordArray[i].length;
+				//alert(word);
+			}
+		}
+		var uniqueWords=Object.keys(wordLengths).length;
+		$('.js-unique-wordcount').text(uniqueWords);
+		var avgLength=totalCharacters/wordCount;
+		$('.js-average-wordlength').text(avgLength.toFixed(2)+" characters");
+	});
+});
+
+function getTokens(rawString) {
+  return rawString.toLowerCase().split(/[ ,!.";:-]+/).filter(Boolean).sort();
+}
