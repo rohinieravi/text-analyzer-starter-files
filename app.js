bfd145b1d8a4e0aa4eec66a67cf3247cedc7f9bc
Rohini Ravindranath <rohinieravi@yahoo.com>
2017-04-17 15:42:05 -0700

    review fixes

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
-$(function(){
-	$('form').submit(function(event){
+$(function() {
+	$('form').submit(function(event) {
 		event.preventDefault();
-		$('.text-report').removeClass('hidden');
+
 		var text = $('#user-text').val().replace(/\r?\n|\r/g, " ");
 		var wordArray = getTokens(text);
-		var wordCount =wordArray.length;
+		var wordCount = getWordCount(wordArray);
+
+		$('.text-report').removeClass('hidden');
 		$('.js-wordcount').text(wordCount);
-		var wordLengths={};
-		var totalCharacters = 0;
-		for(var i=0;i<wordArray.length;i++){
-			if(wordArray[i] in wordLengths){
-				totalCharacters+=wordArray[i].length;
-			}
-			else{
-				wordLengths[wordArray[i]]=wordArray[i].length;
-				totalCharacters+=wordArray[i].length;
-			}
-		}
-		var uniqueWords=Object.keys(wordLengths).length;
-		$('.js-unique-wordcount').text(uniqueWords);
-		var avgLength=totalCharacters/wordCount;
-		$('.js-average-wordlength').text(avgLength.toFixed(2)+" characters");
+		$('.js-unique-wordcount').text(getUniqueWordCount(wordArray));
+		$('.js-average-wordlength').text(getAverageWordLength(wordArray)+" characters");
 	});
 });
 
 function getTokens(rawString) {
   return rawString.toLowerCase().match(/\b[^\s]+\b/g).sort();
+}
+
+function getWordCount(wordArray) {
+	return wordArray.length;
+}
+
+function getUniqueWordCount(wordArray) {
+	var wordLengths = {};
+
+	for ( var i = 0; i < wordArray.length; i++ ) {
+		if( !(wordArray[i] in wordLengths) ) {
+			wordLengths[wordArray[i]]=wordArray[i].length;
+		}
+	}
+	return Object.keys(wordLengths).length;
+}
+
+function getAverageWordLength(wordArray) {
+	var totalCharacters = 0;
+
+	for ( var i = 0; i < wordArray.length; i++ ) {
+		totalCharacters += wordArray[i].length;
+	}
+	return (totalCharacters/wordArray.length).toFixed(2);
 }
